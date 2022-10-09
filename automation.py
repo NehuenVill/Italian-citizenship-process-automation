a3813597cf6df0b0bb6703bea41a9bed51d002fb
JohnSolutions <jonhsmithd930@gmail.com>
2022-10-09 14:50:46 -0300

    Updated automation.py
    Added user_info.json to store the user information.

--- automation.py
+++ automation.py
@@ -1,3 +1,6 @@
+from pickle import FALSE
+import smtplib
+from sre_constants import SUCCESS
 from selenium import webdriver
 from time import sleep
 from selenium.webdriver.common.by import By
@@ -5,23 +8,41 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.alert import Alert
-import smtplib, ssl
 import imaplib
 import email
-from email.header import decode_header
-from datetime import datetime
+import ssl
+from datetime import date, datetime
 from getpass import getpass
+from bs4 import BeautifulSoup
+import json
 
-e_mail = 'nehuenv620@gmail.com'
 url = 'https://prenotami.esteri.it/Home'
 
-options_list = {'sc'   : 'SERVICIOS CONSULARES',
+e_mail = ''
+google_app_pass = ''
+itgov_pass = ''
+mail_pass = ''
+option = ''
+
+with open('user_info.json') as f:
+
+    info = json.load(f)
+
+    e_mail = info['email']
+    google_app_pass = info['goggle app']
+    itgov_pass = info['Italian government website password']
+    mail_pass = info['email password']
+    option = info['option']
+
+options_list = {
+                'sc'   : 'SERVICIOS CONSULARES',
                 'div'  : 'DOCUMENTOS DE IDENTIDAD Y DE VIAJE',
                 'vfue' : ['VISADOS', 'Solamente familiares de ciudadanos UE'],
                 'l'    : 'LEGALIZACIONES',
                 'vtt'  : ['VISADOS', 'Todas tipologias de Visados'],
                 'rpec' : 'REGISTRO DE LA POBLACIÓN Y ESTADO CIVIL',
                 'vdvv' : ['VISADOS', 'Declaraciones de Valor y Visados de Estudio - Estudiantes con preinscripciòn Universitaly']}
+    
 
 def send_mail_notification(email, passw, msg):
 
@@ -29,67 +50,85 @@ def send_mail_notification(email, passw, msg):
 
     context = ssl.create_default_context()
 
-    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+    with smtplib.SMTP_SSL("outlook.office365.com", port, context=context) as server:
         
         server.login(email, passw)
        
         server.sendmail(email, email, msg)
 
-def find_email_code(email, password):
+def find_email_code(mail, password):
 
     imap_server = "outlook.office365.com"
-    imap = imaplib.IMAP4_SSL(imap_server)
-    imap.login(email, password)
+    code = ''
 
-    while True:
+    while code == '':
 
-        status, messages = imap.select("INBOX")
+        sleep(5)
 
-        last_message = int(messages[0])
+        imap = imaplib.IMAP4_SSL(imap_server, port=993)
+        imap.login(mail, password)
 
-        res, msg = imap.fetch(last_message, "(RFC822)")
+        print('Trying to find the code...')
 
-        msg = email.message_from_bytes(msg[0][1])
+        imap.select("INBOX")
+        
+        _, messages = imap.search(None, "ALL")
 
-        subject, encoding = decode_header(msg["Subject"])[0]
+        email_nums = messages[0].split()
 
-        subject = subject.decode(encoding)
+        last_mail_num = email_nums[len(email_nums)-1]
 
-        print(f"last mail subject: {subject}")
-        print('*'*50,)
+        _, data = imap.fetch(last_mail_num, '(RFC822)')
+            
+        msg = email.message_from_bytes(data[0][1])
 
-        if subject == 'OTP Code':
+        subject = msg.get('Subject')
 
-            message = msg.walk()[0].get_payload(decode = True).decode()
+        body = ''
 
-            code = message.replace('OTP Code:', '')
+        for part in msg.walk():
 
-            break
+            if part.get_content_type() == "text/html":
+                
+                body += part.as_string()
+        print(body)
 
-        else:
+        body = body.split('MIME-Version: 1.0\n')[1]
 
-            sleep(1)
+        soup = BeautifulSoup(body, 'html.parser')
 
-            continue
+        print(f'Subject: {subject}')
 
-    return code    
+        for paragraph in soup.find_all('p'):
 
-# Note to myself: Replace all the WebDriverWait with driver.find_element, since I've just realized,
-# that the first one doesn't return the element after waiting for its existence.
+            print(paragraph.text)
 
-def automate(url, email):
+            if 'OTP Code:' in paragraph.text:
+                
+                code = paragraph.text.replace('OTP Code:', '')
 
-    hour = datetime.now().hour
+                break
+        
+        imap.close()  
+        imap.logout()
+    
+    print(f'The code is: {code}')
+
+    return code
+
+
+
+def automate(url, email, password, user_pass, email_pass, opcion):
 
     print('......................Comenzando proceso......................')
 
-    password = getpass('Contraseña para aplicaciones de google: ')
+    #password = getpass('Contraseña para aplicaciones de google: ')
 
-    user_pass = getpass('Contraseña para la página del gobierno italiano: ')
+    #user_pass = getpass('Contraseña para la página del gobierno italiano: ')
 
-    email_pass = getpass('Contraseña de tu e-mail: ')
+    #email_pass = getpass('Contraseña de tu e-mail: ')
 
-    opcion = getpass('Opcion: ')
+    #opcion = getpass('Opcion: ')
 
     driver = webdriver.Chrome()
 
@@ -121,157 +160,220 @@ def automate(url, email):
 
     spanish.click()
 
-    WebDriverWait(driver, 5).until(EC.presence_of_element_located, (By.TAG_NAME, "tr"))
 
-    sleep(2)
+    try:
 
-    rows = driver.find_elements(By.TAG_NAME, 'tr')
-    print('')
-    print(len(rows))
-    print('')
-    for i in range(len(rows)):
+        WebDriverWait(driver, 5).until(EC.presence_of_element_located, (By.TAG_NAME, "tr"))
 
-        if opcion == 'vdvv' or opcion == 'vtt' or opcion == 'vfue':
+        sleep(2)
 
-            print(opcion)
+        rows = driver.find_elements(By.TAG_NAME, 'tr')
+        print('')
+        print(len(rows))
+        print('')
+        for i in range(len(rows)):
 
-            try:
-                
+            if opcion == 'vdvv' or opcion == 'vtt' or opcion == 'vfue':
+
+                print(opcion)
+
+                try:
+                        
+                    properties = driver.find_elements(By.TAG_NAME, 'tr')[i].find_elements(By.TAG_NAME, 'td')
+
+                except Exception:
+
+                    print('Not actual field')
+                    continue
+
+                if properties[0].text == options_list[opcion][0] and properties[2].text == options_list[opcion][1]:
+
+                    book_btn = properties[3].find_element(By.PARTIAL_LINK_TEXT, '/Services/Booking')
+
+                    book_btn.click()
+
+                    break
+                    
+                else:
+                    continue
+
+            else:
+
+                print(opcion)
+            
                 properties = driver.find_elements(By.TAG_NAME, 'tr')[i].find_elements(By.TAG_NAME, 'td')
 
-            except Exception:
+                if len(properties) == 0:
 
-                print('Not actual field')
-                continue
+                    print('Not field')
 
-            if properties[0].text == options_list[opcion][0] and properties[2].text == options_list[opcion][1]:
+                    continue
+                    
+                if properties[0].text == options_list[opcion]:
 
-                book_btn = properties[3].find_element(By.PARTIAL_LINK_TEXT, '/Services/Booking')
+                    book_btn = properties[3].find_element(By.TAG_NAME, 'button')
 
-                book_btn.click()
+                    book_btn.click()
 
-                break
+                    break
+
+                else:
+
+                    continue
+
+        element = WebDriverWait(driver, 10).until(EC.presence_of_element_located, (By.XPATH, "//input[@type='checkbox']"))
+
+        check = driver.find_element(By.XPATH, "//input[@type='checkbox']")
+
+        continue_btn = driver.find_element(By.ID, 'btnAvanti')
+
+        check.click()
+
+        continue_btn.click()
+
+        WebDriverWait(driver, 5).until(EC.alert_is_present())
             
-            else:
-                continue
+        driver.switch_to.alert.accept()
 
-        else:
+        element = WebDriverWait(driver, 10).until(EC.presence_of_element_located, (By.CLASS_NAME, 'day.availableDay'))
 
-            print(opcion)
-    
-            properties = driver.find_elements(By.TAG_NAME, 'tr')[i].find_elements(By.TAG_NAME, 'td')
+        book = driver.find_element(By.ID, "btnPrenota")
 
-            if len(properties) == 0:
+        sleep(1)
 
-                print('Not field')
+        dates = driver.find_elements(By.CLASS_NAME, 'day.availableDay')
 
-                continue
-            
-            if properties[0].text == options_list[opcion]:
+        selected_date = driver.find_element(By.CLASS_NAME, 'day.active')
 
-                book_btn = properties[3].find_element(By.TAG_NAME, 'button')
+        selected_day = selected_date.text
 
-                book_btn.click()
+        print(selected_day)
 
-                break
+        available_dates = []
 
-            else:
+        month = driver.find_element(By.XPATH, "//th[@title='Select Month']").text.replace(' 2022', '')
 
-                continue
+        for date in dates:
+                
+            date_day = date.text
 
-    element = WebDriverWait(driver, 10).until(EC.presence_of_element_located, (By.XPATH, "//input[@type='checkbox']"))
+            final_date = f'{date_day} de {month}, 2022'
 
-    check = driver.find_element(By.XPATH, "//input[@type='checkbox']")
+            available_dates.append(final_date)
 
-    continue_btn = driver.find_element(By.ID, 'btnAvanti')
+        if selected_date.get_attribute('class') == 'day disabled notAvailableDay active':
 
-    check.click()
+            selected_date = dates[0]
 
-    continue_btn.click()
+            selected_date.click()
 
-    WebDriverWait(driver, 5).until(EC.alert_is_present())
-        
-    driver.switch_to.alert.accept()
+        print(f'theres {len(dates)} available dates.')
 
-    element = WebDriverWait(driver, 10).until(EC.presence_of_element_located, (By.CLASS_NAME, 'day.availableDay'))
+        app_date = f'{selected_day} de {month}, 2022'
 
-    book = driver.find_element(By.ID, "btnPrenota")
+        sleep(2)
 
-    sleep(1)
+        book.click()
 
-    dates = driver.find_elements(By.CLASS_NAME, 'day.availableDay')
+        # input#idOtp, button.btn.btn-blue (ok button)
 
-    selected_date = driver.find_element(By.CLASS_NAME, 'day.active')
+        element = WebDriverWait(driver, 10).until(EC.presence_of_element_located, (By.ID, 'idOtp'))
 
-    selected_day = selected_date.text
+        sleep(1)
 
-    print(selected_day)
+        code_input = driver.find_element(By.ID, 'idOtp')
 
-    available_dates = []
+        ok_btn = driver.find_element(By.CLASS_NAME, 'btn.btn-blue')
 
-    month = driver.find_element(By.XPATH, "//th[@title='Select Month']").text.replace(' 2022', '')
+        code = find_email_code(email, email_pass)
 
-    for date in dates:
-            
-        date_day = date.text
+        code_input.send_keys(code)
 
-        final_date = f'{date_day} de {month}, 2022'
+        ok_btn.click()
 
-        available_dates.append(final_date)
+        element = WebDriverWait(driver, 5).until(EC.presence_of_element_located, (By.CLASS_NAME, 'button.primary'))
 
-    if selected_date.get_attribute('class') == 'day disabled notAvailableDay active':
+        back_btn = driver.find_element(By.CLASS_NAME, 'button.primary')
 
-        selected_date = dates[0]
+        back_btn.click()
 
-        selected_date.click()
+        WebDriverWait(driver, 35).until(EC.presence_of_element_located, (By.CLASS_NAME,'button.primary'))
 
-    print(f'theres {len(dates)} available dates.')
+        msg = f"""
+        El proceso de reclamar un turno para {options_list[opcion][0] if len(options_list[opcion]) == 2 else options_list[opcion]} ha sido terminado satisfactoriamente,
+        Este turno quedó agendado para el día {app_date}.
+        Otras fechas disponibles son:
+        {available_dates}
+        """
 
-    app_date = f'{selected_day} de {month}, 2022'
+        send_mail_notification(email, password, msg)
 
-    sleep(2)
+        print('......................Proceso terminado......................')
 
-    book.click()
+        return True
 
-    # input#idOtp, button.btn.btn-blue (ok button)
+    except Exception:
 
-    element = WebDriverWait(driver, 10).until(EC.presence_of_element_located, (By.ID, 'idOtp'))
+        print('No hay turno disponible.')
 
-    sleep(1)
+        not_available = driver.find_element(By.CLASS_NAME, 'jconfirm-buttons')
 
-    code_input = driver.find_element(By.ID, 'idOtp')
+        not_available.click()
 
-    ok_btn = driver.find_element(By.CLASS_NAME, 'btn.btn-blue')
+        driver.close()
 
-    code = find_email_code(email, email_pass)
+        driver.quit()
 
-    code_input.send_keys(code)
+        return False
 
-    ok_btn.click()
 
-    element = WebDriverWait(driver, 5).until(EC.presence_of_element_located, (By.CLASS_NAME, 'button.primary'))
+def scheduler(func):
 
-    back_btn = driver.find_element(By.CLASS_NAME, 'button.primary')
+    while True:
 
-    back_btn.click()
+        succes = func(url, e_mail)
 
-    WebDriverWait(driver, 35).until(EC.presence_of_element_located, (By.CLASS_NAME,'button.primary'))
+        if succes:
 
-    msg = f"""
-    El proceso de reclamar un turno para {options_list[opcion][0] if len(options_list[opcion]) == 2 else options_list[opcion]} ha sido terminado satisfactoriamente,
-    Este turno quedó agendado para el día {app_date}.
-    Otras fechas disponibles son:
-    {available_dates}
-    """
+            break
 
-    send_mail_notification(email, password, msg)
+        now = datetime.now()
 
-    print('......................Proceso terminado......................')
+        hour = now.hour
+
+        minute = now.minute
+
+        time_next_run = None
+
+        if minute >= 30:
+
+            next_run_hour = hour + 1
+            next_run_minute = 0
+
+            next_run = datetime(now.year, now.month, now.day, next_run_hour, next_run_minute, 0)
+
+            time_diff = now - next_run
+
+            time_next_run = time_diff.total_seconds()
+
+        elif minute < 30:
+
+            next_run_hour = hour
+            next_run_minute = 30
+
+            next_run = datetime(now.year, now.month, now.day, next_run_hour, next_run_minute, 0)
+
+            time_diff = now - next_run
+
+            time_next_run = time_diff.total_seconds()
+
+        sleep(time_next_run)
 
 
 
 
 if __name__ == '__main__':
     
-    automate(url, e_mail)
+    #automate(url, e_mail)
 
+    print(find_email_code(e_mail, 'Italia2022'))
